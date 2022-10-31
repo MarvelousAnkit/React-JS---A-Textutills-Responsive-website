05787b31a6eea6b0b45bfd120caf065e0de5ebb4
Ankit  Singh <110014127+MarvelousAnkit@users.noreply.github.com>
2022-11-01 00:03:34 +0530

    Update Contact.js

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -12,15 +12,36 @@ function Contact(props) {
   }
     
   return (
-    <form action="#" method="POST"> {`{% csrf_token %}`}
-
+    <form className='mx-5 my-3' action='#' method='post'  >
+     
     
-
     
-       <input type="text" name="name" id="" />  <br />
-       Email <input type="text" name="email" id="" /> <br />
-       <input type="submit" value="submit"/>
-    </form>
+      
+    <div class="mb-3" >
+    <h1 class="mb-3">
+        Contact US
+      </h1>
+      <label htmlFor="name" class="form-label" >Name</label>
+      <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name='name' style={mystyle} />
+  
+      <label htmlFor="email" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name='email' style={mystyle} />
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  
+    </div>
+    <div class="mb-3" >
+      <label htmlhtmlFor="text" > Enter Your Message</label>
+      <br />
+    <textarea className='my-2' style={mystyle}  name="text" id="text" cols="133" rows="10"></textarea>
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+      <label class="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+    </div>
+    
+    <button type="submit" class="btn btn-dark" style={{backgroundColor : props.mode==='dark'?'black' : 'white', color: props.mode==="dark"? 'white':"black"  }   } >Submit</button>
+  
+  </form>
   )
 }
 
